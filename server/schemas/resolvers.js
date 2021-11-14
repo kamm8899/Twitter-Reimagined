082ece8c01f9cd6e2b0db4824bc21f4dc8b8b8e6
kamm8899 <kamm8899@gmail.com>
2021-11-14 12:50:47 -0800

    resolved issue in resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,65 +1,64 @@
-const { Post, Comment } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
+const { Post, Comment } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
-    Query: {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await userData
+          .findOne({ _id: context.user._id })
+          .select("-__V -password")
+          .populate("post")
+          .populate("comments");
 
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await userData.findOne({ _id: context.user._id })
-                    .select('-__V -password')
-                    .populate('thoughts')
-                    .populate('comments')
-
-                return userData
-            }
-        },
-
-        posts: async (parent, { username }) => {
-            const params = username ? { username } : {};
-            return Post.find(params).sort({ createdAt: -1 });
-        },
-
-        post: async (parent, { _id }) => {
-            return Post.findOne({ _id });
-        },
-
-        users: async () => {
-            return User.find()
-                .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts');
-        },
-
-        user: async (parent, { username }) => {
-            return User.findOne({ username })
-                .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts');
-        }
+        return userData;
+      }
     },
 
-    Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            return user;
-        },
+    posts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Post.find(params).sort({ createdAt: -1 });
+    },
 
-        addPost: async (parent, args, context) => {
-            if (context.user) {
-                const post = await Post.create(args);
+    post: async (parent, { _id }) => {
+      return Post.findOne({ _id });
+    },
 
-                await User.findByIdAndUpdate(
-                    { _id: context.user._id },
-                    { $push: { thoughts: thought._id } },
-                    { new: true }
-                );
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("post");
+    },
 
-                return post;
-            },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("post");
+    },
+  },
 
-        // throw authentication error here
-    }
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      return user;
+    },
+
+    addPost: async (parent, args, context) => {
+      if (context.user) {
+        const post = await Post.create(args);
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+
+        return post;
+      }
+
+      // throw authentication error here
+    },
+  },
 };
 
 module.exports = resolvers;
