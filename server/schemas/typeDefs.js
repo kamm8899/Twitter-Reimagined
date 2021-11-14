6309de9af1f84b2f42bbe1189e1150b228362cac
kamm8899 <kamm8899@gmail.com>
2021-11-14 09:49:41 -0800

    added TypeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,7 +6,32 @@ type User {
     username: String
     email: String
   }
-`
+
+type Comments {
+  _id: ID
+  commentBody: String
+  createdAt: String
+  username: String
+}
+type Auth {
+  token: ID!
+  user: User
+}
+type Query {
+  me: User
+  users: [User]
+  user(username: String!): User
+  comments(username: String): [Thought]
+  comments(_id: ID!): Thought
+}
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  addPost(thoughtText: String!): Thought
+  addComment(thoughtId: ID!, reactionBody: String!): Thought
+  addFriend(friendId: ID!): User
+}
+`;
 ;
 
 module.exports = typeDefs;
