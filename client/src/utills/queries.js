7165d8e1f1ca66a4ae02afd664ee92b2df1c459f
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-20 10:39:30 -0500

    added ME query

---
+++ client/src/utills/queries.js
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql
+`
+{
+     {
+        me {
+          _id
+          username
+         email
+          firstName
+          lastName
+          password
+          postCount
+          posts {
+            _id
+            commentCount
+            comments {
+              _id
+              createdAt
+              commentBody
+            }
+          }
+        }
+      }
+}`
