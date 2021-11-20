5c27344de0c94d9f53e4b3f568c5213efd49d516
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-20 10:50:31 -0500

    added mutations

--- client/src/component/loginForm.js
+++ client/src/component/loginForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Button, TextField, CardContent } from '@mui/material';
 import Box from '@mui/material/Box';
+import Auth from '../utils/auth'
 
 const LoginForm = () => {
 
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,58 @@
+import { gql } from '@apollo/client';
+export const LOGIN_USER = gql`
+mutation login ($email: String!, $password: String!) {
+    login(password:$password, email:$email) {
+      token
+      user {
+      password
+      email
+      }
+    }
+  }  
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!, $firstName: String!, $lastName: String!){
+    addUser(username:$username, password:$password, email:$email, firstName: $firstName, lastName: $lastName) {
+    token
+      user {
+        username
+        email
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+export const ADD_POST = gql `
+mutation addPost($postText: String!, $username: String!) {
+    addPost(postText: $postText, username: $username) {
+      _id
+      postText
+      createdAt
+      username
+      commentCount
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql `
+mutation addComment($postId: ID!, $commentBody: String!) {
+	addComment(postId: $postId, commentBody: $commentBody) {
+    _id
+    commentCount
+    comments {
+      _id
+      commentBody
+      createdAt
+      username
+    }
+  }
+}
+`;
+
+
+
+
+export default { LOGIN_USER, ADD_USER, ADD_POST, ADD_COMMENT }
