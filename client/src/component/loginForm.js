837ded87bc6a984f55c2e4dc8ff673c6b0040d5d
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-20 11:57:31 -0500

    troubleshooting integration

--- client/src/component/loginForm.js
+++ client/src/component/loginForm.js
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Grid, Button, TextField, CardContent } from '@mui/material';
 import Box from '@mui/material/Box';
-import Auth from '../utils/auth'
+import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import FormControl from '@mui/material/FormControl';
 
 const LoginForm = () => {
+    const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const [login] =useMutation(LOGIN_USER);
+  
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setUserFormData({ ...userFormData, [name]: value });
 
+    };
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+    
+        // check if form has everything (as per react-bootstrap docs)
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+    
+        try {
+          const data= await login({
+            variables: {...userFormData}
+          });
+          console.log(data);
+          Auth.login(data.data.login.token);
+        } catch (err) {
+          console.error(err);
+          setShowAlert(true);
+        }
+        setUserFormData({
+            username: '',
+            email: '',
+            password: '',
+          });
+        };
     return (
         <Box style={{ maxWidth: 650, margin: "0 auto", padding: "20px 5px" }}>
             <CardContent sx={{fontFamily: "gotham"}}>
-                <form>
+                <form noValidate validated={validated} onSubmit="handleFormSubmit()">
                     <Grid container spacing={1}>
                         <Grid xs={12} item>
                             <TextField
                                 required
                                 error
                                 id="outlined-error-helper-text"
-                                label="Username"
+                                label="Email Address"
                                 fullWidth
-                                color="common"
+                                color="error"
                                 focused
                                 style={{marginBottom:"10px"}}
+                                onChange={handleInputChange}
                             />
                         </Grid>
                         <Grid xs={12} item>
@@ -30,14 +69,14 @@ const LoginForm = () => {
                                 id="outlined-error-helper-text"
                                 label="Password"
                                 fullWidth
-                                color="common"
+                                color="error"
                                 focused
                                 style={{marginBottom:"10px"}}
-
-                            />
+                                onChange={handleInputChange}
+                                />
                         </Grid>
                         <Grid sx={{ mt: 0.25 }} xs={12} item >
-                            <Button type="submit"  color="secondary" variant="outlined" fullWidth style={{ border: '2px solid', marginBottom:"15px", fontSize:"22px"}}>
+                            <Button type="submit"  color="secondary" variant="outlined" fullWidth style={{ border: '2px solid', marginBottom:"15px", fontSize:"22px"}} disabled={(userFormData.email && userFormData.password)}>
                                 Login
                             </Button>
                         </Grid>
