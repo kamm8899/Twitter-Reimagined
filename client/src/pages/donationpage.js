fe52b4bbe87f47cbc52aa8d2720741234f786618
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-23 13:31:37 -0500

    added different styling

--- client/src/pages/donationpage.js
+++ client/src/pages/donationpage.js
@@ -6,14 +6,13 @@ const Donation = () => {
     
         <Box textAlign="center">
 
-            <Typography variant='h2' gutterbottom color="secondary" align="center">
-                Want to Support Your First Ammendment Rights? Donate Here
-                        
+            <Typography variant='h3' gutterbottom color="secondary" align="center">
+                Want to Support Your First Ammendment Rights? Donate Here  
             </Typography>
-            <Button variant="outlined" color="secondary" href="https://www.paypal.com/donate/?business=HEL6SQD7GGVX4&amount=1&no_recurring=0&item_name=Defend+Your+Free+Speech+Today%21&currency_code=USD">$1.00</Button>
-            <Button variant="outlined" color="secondary" href="https://www.paypal.com/donate/?business=HEL6SQD7GGVX4&amount=5&no_recurring=0&item_name=Defend+Your+Free+Speech+Today%21&currency_code=USD">$5.00</Button>
-            <Button variant="outlined" color="secondary" href="https://www.paypal.com/donate/?business=HEL6SQD7GGVX4&amount=10&no_recurring=0&item_name=Defend+Your+Free+Speech+Today%21&currency_code=USD">$10.00</Button>
-            <Button variant="outlined" color="secondary" href="https://www.paypal.com/donate/?business=HEL6SQD7GGVX4&no_recurring=0&item_name=Defend+Your+Free+Speech+Today%21&currency_code=USD">Custom</Button>
+            <Button variant="outlined" color="error" target="_blank" rel="noopener noreferrer" href="https://www.paypal.com/donate/?business=HEL6SQD7GGVX4&amount=1&no_recurring=0&item_name=Defend+Your+Free+Speech+Today%21&currency_code=USD" sx={{ pr: 4, fontSize:"25px" }}>$1.00</Button>
+            <Button variant="outlined" color="error" target="_blank" rel="noopener noreferrer" href="https://www.paypal.com/donate/?business=HEL6SQD7GGVX4&amount=5&no_recurring=0&item_name=Defend+Your+Free+Speech+Today%21&currency_code=USD" sx={{ pr: 4, fontSize:"25px" }}>$5.00</Button>
+            <Button variant="outlined" color="error" target="_blank" rel="noopener noreferrer" href="https://www.paypal.com/donate/?business=HEL6SQD7GGVX4&amount=10&no_recurring=0&item_name=Defend+Your+Free+Speech+Today%21&currency_code=USD"sx={{ pr: 4, fontSize:"25px" }} >$10.00</Button>
+            <Button variant="outlined" color="error" target="_blank" rel="noopener noreferrer" href="https://www.paypal.com/donate/?business=HEL6SQD7GGVX4&no_recurring=0&item_name=Defend+Your+Free+Speech+Today%21&currency_code=USD" sx={{ pr: 4, fontSize:"25px" }}>Custom</Button>
             <div><img textAlign="center" src={unclechaseDon}></img></div>
             
             
