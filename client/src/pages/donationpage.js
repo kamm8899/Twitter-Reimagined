02d80dd1b5351eef225ec88d6cf1de0ad4508b2c
Pfizz <pfiste80@gmail.com>
2021-11-22 20:32:46 -0500

    added uncle chase

--- client/src/pages/donationpage.js
+++ client/src/pages/donationpage.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Grid, Box, TextField, Button, Card, Typography } from '@mui/material';
-
+import unclechaseDon from '../images/unclechaseDon.png';
 const Donation = () => {
     return (
+    
         <Box textAlign="center">
+
             <Typography variant='h2' gutterbottom color="secondary" align="center">
                 Want to Support Your First Ammendment Rights? Donate Here
+                        
             </Typography>
             <Button variant="outlined" color="secondary">$1.00</Button>
             <Button variant="outlined" color="secondary">$5.00</Button>
             <Button variant="outlined" color="secondary">$10.00</Button>
+            <div><img textAlign="center" src={unclechaseDon}></img></div>
+            
             
         </Box>
        
