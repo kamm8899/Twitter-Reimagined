171b0c5a3c8886ac88d013905937774a68421b58
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-22 19:35:37 -0500

    restored old footer

--- client/src/component/footer.js
+++ client/src/component/footer.js
@@ -1,20 +1,10 @@
 import React from "react";
 import {Typography} from '@mui/material';
-import Jess from '../images/jess.png';
-import Zack from '../images/zack.png';
-import Chase from '../images/unclechase.png';
-import Justin from '../images/justin.png';
-import Mike from '../images/mike.png';
-import Ron from '../images/ron.png';
 function Footer() {
     return (
         <>
       <footer >
-       <Typography color="error" sx={{fontSize:"25px"}}> 
-       This app was made by:
-       </Typography>
-       <img src={Ron} style={{ width:"300px"}}/>
-
+       <Typography color="error">&#9733;&#9733;&#9733;&#9733;&#9733;&#9733;</Typography>
       </footer>
       </>
     )
