04c7b303ab32560a08f627809e2330aaa39519a1
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-22 20:25:20 -0500

    fixed navbar

--- client/src/component/navbar.js
+++ client/src/component/navbar.js
@@ -36,14 +36,12 @@ const Nav = (props) => {
                 {Auth.loggedIn() ? (
                 <>
                   <Button color="secondary" onClick={Auth.logout} sx={{ pr: 4, fontSize:"25px" }}>Logout</Button>
+                  <Button color="secondary" sx={{pr: 4,fontSize:"25px" }} href="/dashboard">Dashboard</Button>
+                  <Button color="secondary" sx={{pr: 4,fontSize:"25px" }}>Donate</Button>
                 </>
               ) : (
                   <Login />
               )}
-                
-                
-                <Button color="secondary" sx={{pr: 4,fontSize:"25px" }}>Donate</Button>
-
                 </Toolbar>
             </AppBar>
         </>
